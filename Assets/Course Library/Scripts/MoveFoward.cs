0a6a5ddd030dbef055a464abc6154206fe449422
JermyJerm <99265510+JermyJerm@users.noreply.github.com>
2022-10-21 11:14:31 -0400

    Initial commit

---
+++ Assets/Course Library/Scripts/MoveFoward.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFoward : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float speed = 40.0f;
+
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
